e9ef7fba13d41d459ee70c6a4440a5dfaac16919
Ramneek Singh <singhrramneek@gmail.com>
2024-10-14 20:23:31 +0530

    updated frontend URL

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:5173",
+		origin: "https://collaboarate.vercel.app",
 		methods: ["GET", "POST"],
 		credentials: true
 	},
